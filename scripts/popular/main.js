5542ec9664b2c3b6ff482febc72841e869c10f17
Franklin Isalguez <frig.ds@gmail.com>
2026-05-27 08:16:44 -0400

    Update popular exports logic

--- scripts/popular/main.js
+++ scripts/popular/main.js
@@ -1,14 +1,5 @@
-const config = { childList: true, subtree: true };
-
-const targetNode = document.getElementById("main-content-div");
-
-const observer = new MutationObserver((mutationList, observer) => {
-  for (const mutation of mutationList) {
-    if (mutation.type === "childList") {
-      createAccountExport();
-      createCreditExport();
-    }
-  }
+window.addEventListener("click", () => {
+  if (document.getElementById("credit-export")) return;
+  createAccountExport();
+  createCreditExport();
 });
-
-observer.observe(targetNode, config);
